7b157cd2247eb38469dff713f5e5823798da992d
jmong143 <jhonlance_37@yahoo.com>
2018-07-30 21:51:50 +0800

    create birthday, greetings and throwback page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Happy Birthday Jeramie Joy Alayon' });
 });
 
+/* GET birthday girl. */
+router.get('/birthday-girl', function(req, res, next) {
+  res.render('birthday-girl', { title: 'Happy Birthday Jeramie Joy Alayon' });
+});
+
+/* GET throwback. */
+router.get('/throwback', function(req, res, next) {
+  res.render('throwback', { title: 'Throwback - Happy Birthday Jeramie Joy Alayon' });
+});
+
+/* GET greetings. */
+router.get('/greetings', function(req, res, next) {
+  res.render('greetings', { title: 'Greetings - Happy Birthday Jeramie Joy Alayon' });
+});
+
 module.exports = router;
