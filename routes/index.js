4c6fcd9c6a1ac9e516f9e143f09ebbdedf2bd510
jmong143 <jhonlance_37@yahoo.com>
2018-09-05 10:10:51 +0800

    latest version

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,26 @@ router.get('/throwback', function(req, res, next) {
 
 /* GET greetings. */
 router.get('/greetings', function(req, res, next) {
-  res.render('greetings', { title: 'Greetings - Happy Birthday Jeramie Joy Alayon', navbar: navbarHeader });
+
+  let greetingsData = {
+    "all": [
+      {"id": "1", "name": "Jocelyn Mercado", "class": "btn-primary", "greetUrl1": "/img/greetings/message/jocelyn-mercado-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/jocelyn-mercado-self.png"},
+      {"id": "2", "name": "John Joseph Dagonio", "class": "btn-danger", "greetUrl1": "/img/greetings/message/john-joseph-dagonio-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "https://youtube.com", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/john-joseph-dagonio-self.png"},
+      {"id": "3", "name": "Jennifer Almoete", "class": "btn-danger", "greetUrl1": "/img/greetings/message/jennifer-almoete-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/jennifer-almoete-self.png"},
+      {"id": "4", "name": "Christian Osias", "class": "btn-danger", "greetUrl1": "/img/greetings/message/christian-osias-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/christian-osias-self.png"},
+      {"id": "5", "name": "Reynald Nabur", "class": "btn-danger", "greetUrl1": "/img/greetings/message/reynald-nabur-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/reynald-nabur-self.png"},
+      {"id": "6", "name": "Lorelyn Caballas", "class": "btn-danger", "greetUrl1": "/img/greetings/message/lorelyn-caballas-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/lorelyn-caballas-self.png"},
+      {"id": "7", "name": "Maicha De Ocampo", "class": "btn-danger", "greetUrl1": "/img/greetings/message/maicha-de-ocampo-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/maicha-de-ocampo-self.png"},
+      {"id": "8", "name": "Louie Briones", "class": "btn-danger", "greetUrl1": "/img/greetings/message/louie-briones-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/louie-briones-self.png"},
+      {"id": "9", "name": "Dian Capal", "class": "btn-danger", "greetUrl1": "/img/greetings/message/dian-capal-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/dian-capal-self.png"},
+      {"id": "10", "name": "Jireh Joy Alayon", "class": "btn-danger", "greetUrl1": "/img/greetings/message/bam-greet1.png", "greetUrl2": "/img/greetings/message/bam-greet2.png", "greetUrl3": "/img/greetings/message/bam-greet3.png", "videoUrl": "", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/bam-self.png"},
+      {"id": "11", "name": "Sherry Mae Manuel", "class": "btn-danger", "greetUrl1": "/img/greetings/message/sherry-greet.png", "greetUrl2": "", "greetUrl3": "", "videoUrl": "http://youtube.com", "videoUrl2": "", "videoUrl3": "", "picUrl": "/img/greetings/dp/sherry-self.png"},
+      {"id": "12", "name": "Marvin and Bel", "class": "btn-danger", "greetUrl1": "", "greetUrl2": "", "greetUrl3": "", "videoUrl": "http://youtube.com", "videoUrl2": "http://youtube.com", "videoUrl3": "http://youtube.com", "picUrl": "/img/greetings/dp/marbel-self.png"},
+      
+      
+    ]
+  }
+  res.render('greetings', { title: 'Greetings - Happy Birthday Jeramie Joy Alayon', greetingsData: greetingsData });
 });
 
 module.exports = router;
